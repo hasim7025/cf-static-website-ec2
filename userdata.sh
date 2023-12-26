410b28c9d01a8651f627b83dba5231e30ed84e1c
hasim7025 <kochasim25@gmail.com>
2023-12-26 18:31:16 +0100

    added userdata script

---
+++ userdata.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+dnf update -y
+dnf install httpd -y
+FOLDER="https://raw.githubusercontent.com/hasim7025/cfn-static-website-ec2/main/static-web"
+cd /var/www/html
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+wget ${FOLDER}/cat3.png
+systemctl start httpd
+systemctl enable httpd
